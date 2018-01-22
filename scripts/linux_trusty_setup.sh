100ccda174d58ee1021f9ad88e05bbbd7318d468
robin saxifrage <robinsaxifrage@live.ca>
2018-01-21 23:59:54 -0800

    build setup

--- scripts/linux_trusty_setup.sh
+++ scripts/linux_trusty_setup.sh
@@ -1,6 +1,5 @@
 #	Package install script for Ubuntu 14.04 LTS
 
-
 echo "Installing Python 3.6. and Pip"
 #   Add the Python 3.6 backport repository (Thanks Jon.).
 add-apt-repository ppa:jonathonf/python-3.6 -y
@@ -12,10 +11,10 @@ curl https://bootstrap.pypa.io/get-pip.py | python3.6
 
 echo "Installing Python package dependencies."
 #   Install package requirements.
-/usr/bin/yes |  python3.6 -m pip install -r ./requirements.txt
+/usr/bin/yes | sudo python3.6 -m pip install -r ./requirements.txt
 
 #   Create settings if it doesn't exist.
-if [! -f ./settings.json ]; then
+if [ ! -f ./settings.json ]; then
 	echo "Creating default settings; DO NOT USE THIS CONFIGURATION IN PRODUCTION."
 	cp -f ./default_settings.json ./settings.json
 fi
