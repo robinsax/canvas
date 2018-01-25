915fb06334f9b85fa492a88708430ad224b6f141
robinsax <robinsaxifrage@live.ca>
2018-01-25 14:01:55 -0800

    unrefactor to fix

--- scripts/ubuntu_trusty_setup.sh
+++ scripts/ubuntu_trusty_setup.sh
@@ -1,4 +1,17 @@
-#	Package setup script for Ubuntu 14.04 LTS
+#	Package install script for Ubuntu 14.04 LTS
+
+echo "Installing Python 3.6 and Pip"
+#   Add the Python 3.6 backport repository (Thanks Jon.).
+add-apt-repository ppa:jonathonf/python-3.6 -y
+apt-get update
+#   Install Python.
+apt-get install python3.6 postgresql -y
+#   Install Pip.
+curl https://bootstrap.pypa.io/get-pip.py | python3.6
+
+echo "Installing Python package dependencies"
+#   Install package requirements.
+/usr/bin/yes | sudo python3.6 -m pip install -r ./requirements.txt
 
 #   Create settings if it doesn't exist.
 if [ ! -f ./settings.json ]; then
