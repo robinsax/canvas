4e22e891a30bce3d269f608fc4d6c806a45a5b4a
robinsax <robinsaxifrage@live.ca>
2018-02-14 14:52:53 -0800

    update tests

--- tests/combined.py
+++ tests/combined.py
@@ -72,8 +72,8 @@ def test_basic():
 	), 'base.css serves')
 
 	check((
-		client.get('/assets/core.js').status_code == 200
-	), 'core.js serves')
+		client.get('/assets/core.min.js').status_code == 200
+	), 'core.min.js serves')
 
 	check_html_response(client.get('/'), 200, 
 			lambda html: 'Hello World!' in html, 'Hello world page serves')
