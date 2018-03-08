e202b9208599e06f272d00af43e98163eb334070
robinsax <robinsaxifrage@live.ca>
2018-03-08 01:32:57 -0800

    fix build?

--- etc/scripts/install_dependencies.sh
+++ etc/scripts/install_dependencies.sh
@@ -3,7 +3,7 @@ echo "Installing canvas dependencies..."
 apt-get update
 apt-get install python3-pip postgresql nodejs -y
 #	Install Python packages.
-pip3 install -r requirements.txt -y
+/usr/bin/yes | pip3 install -r requirements.txt
 #	Install CoffeeScript, less, and Babel.
 npm install --save-dev coffeescript babel-cli babel-preset-es2015
 
