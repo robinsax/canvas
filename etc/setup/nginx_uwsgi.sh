a3f15339f1339671c817125b38ec9c16742dd152
robinsax <robinsaxifrage@live.ca>
2018-03-31 23:25:58 -0700

    more updates

--- etc/setup/nginx_uwsgi.sh
+++ etc/setup/nginx_uwsgi.sh
@@ -4,14 +4,13 @@ apt-get update
 apt-get install uwsgi nginx -y
 
 #	Configure uWSGI.
-cp ./etc/configurations/canvas_uwsgi.ini ./canvas_uwsgi.ini
-cat ./etc/configurations/canvas_uwsgi.conf | sed "s/@user/$1/g" > /etc/init/canvas.conf
+cp ./etc/configurations/uwsgi_config.ini ./canvas_uwsgi.ini
+cat ./etc/configurations/uwsgi_config.conf | sed "s/@user/$1/g" > /etc/init/canvas.conf
 
 #   Configure nginx.
 cat ./etc/configurations/nginx_config | sed "s/@user/$1/g" | sed "s/@domain/$2/g" > /etc/nginx/sites-available/canvas
 ln -s /etc/nginx/sites-available/canvas /etc/nginx/sites-enabled
 
-mkdir /var/log/uwsgi
 touch /var/log/uwsgi/all.log
 #   Setup uWSGI logging.
 chmod a+w /var/log/uwsgi/all.log
