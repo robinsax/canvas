5602ef883bfd5abd1ac699f056d272cefdd352da
robinsax <robin_m_saxifrage@ikn-2.c.northern-center-175123.internal>
2018-04-18 03:33:19 +0000

    add service test

--- tests/__init__.py
+++ tests/__init__.py
@@ -8,3 +8,14 @@ import canvas.tests as cvt
 from . import (
 	dictionaries
 )
+
+@cvt.test('Basic asset service')
+def test_asset_service():
+	client = cvt.create_client()
+
+	response = client.get('/assets/canvas.js')
+	cvt.assertion(
+		'canvas.js serves',
+		response.status_code == 200
+	)
+
