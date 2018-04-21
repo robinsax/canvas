a3c031f2ab6c6c8ffbb39857d8b87ef1079c2df2
robinsax <robinsaxifrage@live.ca>
2018-04-21 11:55:29 -0700

    fuck it

--- etc/install_dependencies.sh
+++ etc/install_dependencies.sh
@@ -1,12 +1,8 @@
 #	Patch versioning issue in CI. Plugin builds will break if we purely bump
 #	postgres in Travis config.
-sudo service postgresql stop
-
 echo "Installing canvas dependencies..."
 
 apt-get update
 apt-get install python3-pip postgresql nodejs npm -y
-
-sudo service postgresql start
 
 echo "Done"
