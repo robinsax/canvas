8dbc9def8d966feaf54209b0d53ab0985e6eaae2
rsaxifrage <Robin.Saxifrage@leg.bc.ca>
2018-04-25 15:07:47 -0700

    migrate forms and views

--- assets/client/core/views.jsx
+++ assets/client/core/views.jsx
@@ -6,32 +6,34 @@ class ViewPart {
 		core.view = (name, definition) => this.view(name, definition);
 		core.views = this.views = {};
 
-		core.utils.resolveEvents = (instance, cls, el) => this.resolveEvents(instance, cls, el);
+		core.utils.resolveEventsAndInspections = (instance, cls, el) => this.resolveEventsAndInspections(instance, cls, el);
 		core.event = (on, selector=null) => this.viewEvent(on, selector);
+		core.inspects = selector => this.viewInspect(selector);
 
-		this.defineDefaultViews(core);
+		this.defineDefaultViews();
 
 		tk(window).on('load', () => this.createViews());
 	}
 
-	resolveEvents(instance, cls, el) {
-		core.utils.iterateMethodDecorated(cls, '_events', eventDesc => {
-			if (el.is(eventDesc.selector)) {
-				el.on(eventDesc.on, (tel, event) => {
-					instance[eventDesc.key](tel, event);
-				});	
-			}
-			el.children(eventDesc.selector).on(eventDesc.on, (tel, event) => {
+	resolveEventsAndInspections(instance, cls, el) {
+		core.utils.iterateDecoratedMethods(cls, '_events', eventDesc => {
+			el.reduce(eventDesc.selector).on(eventDesc.on, (tel, event) => {
 				instance[eventDesc.key](tel, event);
 			});
 		});
+		core.utils.iterateDecoratedMethods(cls, '_inspectors', desc => {
+			el.reduce(desc.selector).iter(tel => { instance[desc.key](tel); });
+		});
 	}
 
-	view(name, options) {
+	view(options) {
 		return (ViewClass) => {
+			let name = ViewClass.name.toLowerCase();
+
 			class View extends ViewClass {
 				constructor() {
 					super(...arguments);
+					this.name = name;
 					this.updateOptionDefaults = this.updateOptionDefaults || (x => x);
 					core.utils.applyOptionalizedArguments(this, options, this.updateOptionDefaults({
 						state: {},
@@ -41,9 +43,6 @@ class ViewPart {
 						dataSource: null,
 						query: {}
 					}));
-					if (!this.template && this.templates) {
-						this.template = this.templates.root;
-					}
 
 					this.base = {
 						fetch: (then=(() => {})) => {
@@ -55,7 +54,6 @@ class ViewPart {
 								.query(this.query)
 								.success(response => {
 									this.data = response.data;
-									this.render();
 									then();
 								})
 								.send();
@@ -74,12 +72,8 @@ class ViewPart {
 							core.utils.installObjectObservers(this.data, boundRender);
 							core.utils.installObjectObservers(this.state, boundRender);
 
-							this.node = tk.template((...a) => <div>{ this.template(...a) }</div>)
+							this.node = this._templateContext
 								.data(this.data, this.state, this.templates)
-								.live()
-								.inspection(el => {
-									core.utils.resolveEvents(this, ViewClass, el);
-								})
 								.render();
 							
 							this._rendering = false;
@@ -93,7 +87,7 @@ class ViewPart {
 						}
 					});
 					core.utils.invokeDecoratedMethods(this, ViewClass, '_onSetup');
-
+					
 					Object.defineProperties(this, {
 						_rendering: {
 							value: false,
@@ -106,7 +100,13 @@ class ViewPart {
 							enumerable: false
 						}
 					});
+
 					this.node = null;
+					this._templateContext = tk.template(this.template || this.templates.root)
+						.inspection(el => {
+							core.utils.resolveEventsAndInspections(this, ViewClass, el);
+						})
+						.live();
 				}
 			}
 			
@@ -116,32 +116,24 @@ class ViewPart {
 	}
 
 	createViews() {
-		tk('cv-view').iter(el => {
-			let name = el.attr('cv-name'), 
-				ViewClass = this._viewDefinitions[name];
-
-			if (!ViewClass) {
-				tk.warn('No such view: ' + name);
+		core.utils.iterateNonStandardTags((el, viewName) => {
+			if (!this._viewDefinitions[viewName]) {
 				return;
 			}
 
-			let label = el.attr('cv-label') || name,
-				view = new ViewClass();
+			let label = el.attr('data-name') || el.attr('name') || viewName, 
+				view = new this._viewDefinitions[viewName]();
 			this.views[label] = view;
 
 			let create = () => {
 				core.onceReady(() => {
-					tk.log('Created view ' + name + (label != name ? ' (as ' + label + ')' : ''));
 					el.replace(view.render());
+					tk.log('Created view ' + viewName + ' as ' + label);
 				});
-			};
+			}
 			
-			if (view.dataSource){
-				view.fetch(create);
-			}
-			else {
-				create();
-			}
+			if (view.dataSource){ view.fetch(create); }
+			else { create(); }
 		});
 	}
 
@@ -159,8 +151,17 @@ class ViewPart {
 			}
 		});
 	}
+
+	viewInspect(selector) {
+		return core.utils.createMethodDecorator('_inspectors', key => {
+			return {
+				selector: selector,
+				key: key
+			};
+		})
+	}
 	
-	defineDefaultViews(core) {
+	defineDefaultViews() {
 		class ModalView {
 			constructor() {
 				this.isOpen = false;
@@ -177,10 +178,6 @@ class ViewPart {
 			updateOptionDefaults(options) {
 				options.className = null;
 				return options;
-			}
-			
-			template() {
-				return 
 			}
 		
 			@core.event('.modal, .close')
