86569828a5641ae931616c57b517b265694a5323
robinsax <robinsaxifrage@live.ca>
2018-09-17 11:41:48 -0700

    remove nasty logging

--- canvas/core/model/dictizations.py
+++ canvas/core/model/dictizations.py
@@ -73,10 +73,6 @@ def dictize(target, include=tuple(), omit=tuple()):
 		*(target.__class__.__dictized__),
 		*include
 	) if attr not in omit and hasattr(target, attr))
-	#	XXX
-	log = logger('wat')
-	log.debug(str(type(target)))
-	log.debug(target_attrs)
 	
 	dictization = dict()
 	for attr in target_attrs:
